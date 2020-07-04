c11d2cec45517a5dfff34b339ed7c16398fad374
archerckk <archerckk@163.com>
2020-07-04 14:07:18 +0800

    增加部分注释，优化之前的删除用户操作逻辑

--- appium_hw/test_appium_hw.py
+++ appium_hw/test_appium_hw.py
@@ -38,6 +38,12 @@ class TestWeWork:
     def teardown(self):
         self.driver.quit()
 
+    def find(self, *by):
+        return WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable(*by))
+
+    def finds(self, *by):
+        return WebDriverWait(self.driver, 10).until(expected_conditions.presence_of_all_elements_located(*by))
+
     @pytest.mark.parametrize('name,gender,phone', get_data())
     def test_add_member(self, name, gender, phone):
         self.driver.find_element_by_xpath('//*[@class="android.widget.LinearLayout"]//*[@text="通讯录"]').click()
@@ -46,14 +52,18 @@ class TestWeWork:
         self.driver.find_element_by_xpath('//*[@text="姓名　"]/../android.widget.EditText[@text="必填"]').send_keys(name)
         self.driver.find_element_by_xpath('//*[@text="性别"]/..//android.widget.LinearLayout').click()
 
+        # 稳定点击弹出的男女选择框页面，添加显示等待
         WebDriverWait(self.driver, 10).until(expected_conditions.presence_of_all_elements_located(
             (By.XPATH, '//*[contains(@text,"男")or contains(@text,"女")]')))
         if gender == '男':
             self.driver.find_element_by_xpath('//*[@text="男"]').click()
         else:
             self.driver.find_element_by_xpath('//*[@text="女"]').click()
+
         self.driver.find_element_by_xpath('//*[@text="手机号"]').send_keys(phone)
         self.driver.find_element_by_xpath('//*[@text="保存"]').click()
+
+        # 获取添加用户成功的toast
         add_toast_info = self.driver.find_element_by_xpath("//*[@class='android.widget.Toast']").text
 
         assert add_toast_info == '添加成功'
@@ -63,44 +73,32 @@ class TestWeWork:
         self.driver.find_element_by_xpath('//*[@class="android.widget.LinearLayout"]//*[@text="通讯录"]').click()
         self.driver.find_elements_by_xpath('//android.widget.LinearLayout//android.widget.RelativeLayout'
                                            '//android.widget.TextView')[2].click()
-
+        # 跳转页面之后等元素稳定再进行查找元素
         WebDriverWait(self.driver, 10).until(
             expected_conditions.visibility_of_all_elements_located(
                 (By.XPATH, '//android.widget.ListView/android.widget.RelativeLayout')))
 
-        sleep(5)
-
-        member_row_elements: list = self.driver.find_elements_by_xpath(
-            '//android.widget.ListView/android.widget.RelativeLayout')
-        member_row_elements.pop(0)
-
-        length = len(member_row_elements)
-        print('待删除的用户列表长度为:', length)
         print("删除的目标用户为：", name)
-        for i in range(length):
-            cur_username = self.driver.find_elements_by_xpath(
-                '//android.widget.ListView/android.widget.RelativeLayout//android.widget.TextView')[i].text
-            print("获取到的用户名信息为：", cur_username)
+        cur_username_list = self.driver.find_elements(By.XPATH,
+                                                      f'//*[@text="{name}"]')
+        # 在数据已被删除的情况，不进行删除操作
+        if len(cur_username_list) == 0:
+            print('目标用户不在当前用户列表当中')
+        else:
+            cur_username_list[0].click()
+            self.find((By.XPATH, '//*[@text="删除成员"]')).click()
+            self.find((By.XPATH, '//*[@text="确定"]')).click()
 
-            if name == cur_username:
-                image_icon_element_list = member_row_elements[i].find_elements_by_xpath('//android.widget.ImageView')
-                print('icon列表', image_icon_element_list)
-                image_icon_element_list[1].click()
-                self.driver.find_element_by_xpath('//*[@text="删除成员"]').click()
-                self.driver.find_element_by_xpath('//*[@text="确定"]').click()
-            else:
-                continue
+            return_to_member_list_loc = (
+                By.XPATH, "//android.widget.ListView/android.widget.RelativeLayout//android.widget.TextView")
 
-        return_to_member_list_loc = (
-            By.XPATH, "//android.widget.ListView/android.widget.RelativeLayout//android.widget.TextView")
+            # 跳转页面之后等元素稳定再进行查找元素
+            WebDriverWait(self.driver, 10).until(
+                expected_conditions.visibility_of_all_elements_located(return_to_member_list_loc))
 
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.visibility_of_all_elements_located(return_to_member_list_loc))
+            # 获取现在的所有用户的名字
+            new_member_name_list = [name_element.text for name_element in self.finds(return_to_member_list_loc)]
 
-        sleep(5)
+            print('现在的用户列表为：', new_member_name_list)
 
-        new_member_name_elements_list = self.driver.find_elements(*return_to_member_list_loc)
-
-        new_member_name_list = [name_element.text for name_element in new_member_name_elements_list]
-        print('现在的用户列表为：', new_member_name_list)
-        assert name not in new_member_name_list
+            assert name not in new_member_name_list
--- conftest.py
+++ conftest.py
@@ -9,6 +9,54 @@ def prepare():
     print('计算结束')
 
 
+from typing import List
+
+import pytest
+import yaml
+from _pytest.config import Config
+from _pytest.nodes import Item
+from requests import Session
+
+
+@pytest.fixture()
+def prepare():
+    print('开始计算')
+    yield '调用fixture'
+    print('计算结束')
+
+
+def pytest_collection_modifyitems(
+        session: "Session", config: "Config", items: List["Item"]
+) -> None:
+    """ called after collection has been performed, may filter or re-order
+    the items in-place.
+
+    :param _pytest.main.Session session: the pytest session object
+    :param _pytest.config.Config config: pytest config object
+    :param List[_pytest.nodes.Item] items: list of item objects
+    """
+    # 修复中文的ids显示问题
+    for item in items:
+        item.name = item.name.encode('utf-8').decode('unicode-escape')
+        item._nodeid = item._nodeid.encode('utf-8').decode('unicode-escape')
+
+        # 自定义添加标签
+        if 'find' in item.nodeid:
+            item.add_marker(pytest.mark.find)
+        elif 'update' in item.nodeid:
+            item.add_marker(pytest.mark.update)
+
+
+def pytest_configure(config):
+    # config.addinivalue_line(
+    #     "markers", "find: some cases contain find "
+    # )
+    config.addinivalue_line(
+        "markers", "update: some cases contain find"
+    )
+
+
+
 # 注册一个env参数
 def pytest_addoption(parser):
     mygroup = parser.getgroup('hogwarts')  # 先获取一个组名
