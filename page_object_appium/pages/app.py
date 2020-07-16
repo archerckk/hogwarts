2574e9981be4c6f6569f8d33ee0c1ac9d9272c50
archerckk <archerckk@163.com>
2020-07-16 21:07:06 +0800

    提交修改代码

--- page_object_appium/pages/app.py
+++ page_object_appium/pages/app.py
@@ -18,13 +18,16 @@ class App(BasePage):
                 desired_caps = yaml.safe_load(f)['mumu_xueqiu']
                 desired_caps['udid'] = os.getenv('udid', None)
                 # desired_caps['udid'] = 'emulator-5554'
-            self._driver = webdriver.Remote('http://192.168.163.1:4444/wd/hub', desired_caps)
+            self._driver = webdriver.Remote('http://10.8.8.186:4444/wd/hub', desired_caps)
             # self._driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
             self._driver.implicitly_wait(10)
         else:
             self._driver.start_activity(_package, _activity)
 
         return self
+
+    def close(self):
+        self._driver.quit()
 
     def main(self):
         return Main(self._driver)
--- page_object_appium/test_cases/test_search.py
+++ page_object_appium/test_cases/test_search.py
@@ -2,6 +2,11 @@ from page_object_appium.pages.app import App
 
 
 class TestSearch:
+    def setup(self):
+        self.app = App().start()
+
+    def teardown(self):
+        self.app.close()
 
     def test_search_input(self):
-        App().start().main().goto_market().goto_search().search('jd')
+        self.app.main().goto_market().goto_search().search('jd')
