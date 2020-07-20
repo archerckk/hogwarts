634450c674e3aad64d2ae89af3b181c3d6848aea
archerckk <archerckk@163.com>
2020-07-20 15:51:59 +0800

    提交测试代码

--- selenium_test/selenium_grid_demo.py
+++ selenium_test/selenium_grid_demo.py
@@ -9,20 +9,24 @@ class TestSeleniumGridDemo:
     def setup(self):
         self.hub_url = 'http://10.8.8.186:4444/wd/hub'
         self.web_browser = os.getenv('browser', None)
+
         if self.web_browser == 'chrome':
             capbilities = DesiredCapabilities.CHROME.copy()
             self.driver = Remote(command_executor=self.hub_url, desired_capabilities=capbilities)
+
         elif self.web_browser == 'ie':
             capbilities = DesiredCapabilities.INTERNETEXPLORER.copy()
             self.driver = Remote(command_executor=self.hub_url, desired_capabilities=capbilities)
+
         elif self.web_browser == 'edge':
             capbilities = DesiredCapabilities.EDGE.copy()
             # capbilities['platform']='WIN10'
             # capbilities['browser-version']='83'
-            capbilities['executable_path']='msedgedriver.exe'
             # capbilities['visual']='true'
+            capbilities['executable_path'] = 'msedgedriver.exe'
             self.driver = Remote(
                 command_executor=self.hub_url, desired_capabilities=capbilities)
+
         elif self.web_browser == 'firefox':
             capbilities = DesiredCapabilities.FIREFOX.copy()
             self.driver = Remote(command_executor=self.hub_url, desired_capabilities=capbilities)
