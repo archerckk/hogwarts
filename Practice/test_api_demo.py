5d41f5543399b43d0b55bd62fbaa11254f5ccce6
archerckk <archerckk@163.com>
2020-07-30 09:35:17 +0800

    api demo 代码提交

--- Practice/test_api_demo.py
+++ Practice/test_api_demo.py
@@ -1,5 +1,5 @@
 import requests
-import pprint
+from pprint import pprint
 
 
 class TestApiDemo:
@@ -8,10 +8,25 @@ class TestApiDemo:
     def test_get(self):
         payload = {"key1": "value1", "key2": "value2"}
         r = requests.get(f'{self.host}/get', params=payload)
-        pprint.pprint(r.json())
+        pprint(r.json())
 
     def test_post_form(self):
         payload = {"key1": "value1", "key2": "value2"}
 
         r = requests.post(f"{self.host}/post", data=payload)
-        pprint.pprint(r.json())
+        pprint(r.json())
+
+    def test_file_upload(self):
+        files={'file':open('phone.yml','rb')}
+        r=requests.post(f"{self.host}/post",files=files)
+        pprint(r.raw.read())
+
+    def test_post_json(self):
+        payload = {"some": "data"}
+        r = requests.post(f"{self.host}/post", json=payload)
+        pprint(r.json())
+
+    def test_post_xml(self):
+        xml="""<?xml version='10.0' encoding='utf-8'><a>test</a>"""
+        headers={'Content-Type':'application/xml'}
+        r=requests.post(f"{self.host}/post")
