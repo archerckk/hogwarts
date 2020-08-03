80857edbcdfd33d55048c4a65dd4aa4e20c73f26
archerckk <archerckk@163.com>
2020-08-03 19:33:33 +0800

    新增测试代码

--- api_hw2/test_case/test_tag2.py
+++ api_hw2/test_case/test_tag2.py
@@ -1,8 +1,8 @@
 import json
 
 import pytest
-from api_hw2.api.tag import Tag
-from api_hw2.api.wework import WeWork
+from api.tag import Tag
+from api.wework import WeWork
 from filelock import FileLock
 
 
@@ -32,67 +32,62 @@ class TestTag:
                 fn.write_text(json.dumps(data))
         return data
 
-
-
     def setup(self):
-        self.tag=Tag()
+        self.tag = Tag()
 
-    @pytest.mark.parametrize('tagname', ['tagx11', 'taxg21', 'taxg31'])
-    def test_tag_add(self, tagname, token):
-        res = self.tag.tag_add(tagname, token)
-        assert res['errmsg']=='created'
+    def test_tag_add(self, token):
+        print(token)
+        res = self.tag.tag_add('test_tag13', token)
+        print(res)
+        assert res['errmsg'] == 'created'
 
-    # def test_tag_get(self,token):
-    #     res=self.tag.tag_get(2,token)
-    #     assert res['errmsg'] == 'ok'
-    #     assert res['tagname'] is not None
+    def test_tag_get(self, token):
+        print(token)
+        res = self.tag.tag_get(2, token)
+        assert res['errmsg'] == 'ok'
+        assert res['tagname'] is not None
 
-    # def test_tag_update(self,token):
-    #     res=self.tag.tag_update(1,'test_xx',token)
-    #     assert res['errmsg'] == 'updated'
-    #
-    # def test_tag_delete(self,token):
-    #     res = self.tag.tag_delete(1, token)
-    #     assert res['errmsg']=='deleted'
+    def test_tag_update(self, token):
+        print(token)
+        res = self.tag.tag_update(1, 'test_xx', token)
+        # assert res['errmsg'] == 'updated'
 
-    @pytest.mark.parametrize('tagname', ['tagx11', 'taxg21', 'taxg31'])
-    def test_all(self, tagname, token):
-        #新增一个tag
-        try :
-            res = self.tag.tag_add(tagname, token)
+    def test_tag_delete(self, token):
+        print(token)
+        res = self.tag.tag_delete(1, token)
+        # assert res['errmsg']=='deleted'
+
+    def test_all(self, token):
+        # 新增一个tag
+        try:
+            print(token)
+            res = self.tag.tag_add('tag1', token)
             assert res['errmsg'] == 'created'
             self.tagid = res['tagid']
-        # print(res)
         except Exception as e:
             if '40071' in e.__str__():
                 res = self.tag.tag_list_get(token)
-                # print(res)
+                print(res)
                 for i in res['taglist']:
-                    if tagname in i.values():
+                    if 'tag1' in i.values():
                         self.tagid = i['tagid']
                         self.tag.tag_delete(self.tagid, token)
-                res = self.tag.tag_add(tagname, token)
+                res = self.tag.tag_add('tag1', token)
                 assert res['errmsg'] == 'created'
 
         # 查询这个tag是否创建成功
         res = self.tag.tag_get(self.tagid, token)
         assert res['errmsg'] == 'ok'
         assert res['tagname'] is not None
-        #升级这个tag
-        res = self.tag.tag_update(self.tagid, tagname + '1', token)
+        # 升级这个tag
+        res = self.tag.tag_update(self.tagid, 'test_xxx', token)
         assert res['errmsg'] == 'updated'
         # 查询tag是否升级成功
         res = self.tag.tag_get(self.tagid, token)
         assert res['errmsg'] == 'ok'
-        #删除这个tag
+        # 删除这个tag
         res = self.tag.tag_delete(self.tagid, token)
         assert res['errmsg'] == 'deleted'
         # 查询tag是否删除成功
         res = self.tag.tag_get(self.tagid, token)
-        # print(res)
-
-    @pytest.mark.parametrize('tagname', ['tagx11', 'taxg21', 'taxg31'])
-    def test_all2(self, tagname, token):
-        # 新增一个tag
-        res = self.tag.tag_add(tagname, token)
         print(res)
