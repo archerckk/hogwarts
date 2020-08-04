daa1e1767a47878b55cc632a37e660b31cfa29b1
archerckk <archerckk@163.com>
2020-08-04 13:28:45 +0800

    新增注释

--- api_hw2/test_case/test_tag.py
+++ api_hw2/test_case/test_tag.py
@@ -7,6 +7,7 @@ from filelock import FileLock
 
 
 def produce_expensive_data():
+    '生成token的函数'
     we_work = WeWork()
     token = we_work.get_token()
     return token
@@ -15,23 +16,23 @@ def produce_expensive_data():
 class TestTag:
     @pytest.fixture(scope='session')
     def token(self, tmp_path_factory, worker_id):
+        # 假如是单线程运行的话直接返回token
         if not worker_id:
-            # not executing in with multiple workers, just produce the data and let
-            # pytest's fixture caching do its job
             return produce_expensive_data()
 
-        # get the temp directory shared by all workers
+        # 多线程运行先生成一个临时的路径
         root_tmp_dir = tmp_path_factory.getbasetemp().parent
 
         fn = root_tmp_dir / "data.json"
         with FileLock(str(fn) + ".lock"):
             if fn.is_file():
+                #第二次获取的时候读取文件里面的内容
                 data = json.loads(fn.read_text())
             else:
+                #第一次获取的时候读取函数返回的数据并且写入临时存放的文件路径
                 data = produce_expensive_data()
                 fn.write_text(json.dumps(data))
         return data
-
 
 
     def setup(self):
@@ -42,18 +43,19 @@ class TestTag:
         res = self.tag.tag_add(tagname, token)
         assert res['errmsg']=='created'
 
-    # def test_tag_get(self,token):
-    #     res=self.tag.tag_get(2,token)
-    #     assert res['errmsg'] == 'ok'
-    #     assert res['tagname'] is not None
+    def test_tag_get(self, token):
+        res = self.tag.tag_get(2, token)
+        print(res)
+        assert res['errmsg'] == 'ok'
+        assert res['tagname'] is not None
 
-    # def test_tag_update(self,token):
-    #     res=self.tag.tag_update(1,'test_xx',token)
-    #     assert res['errmsg'] == 'updated'
-    #
-    # def test_tag_delete(self,token):
-    #     res = self.tag.tag_delete(1, token)
-    #     assert res['errmsg']=='deleted'
+    def test_tag_update(self, token):
+        res = self.tag.tag_update(1, 'test_xx', token)
+        assert res['errmsg'] == 'updated'
+
+    def test_tag_delete(self, token):
+        res = self.tag.tag_delete(1, token)
+        assert res['errmsg'] == 'deleted'
 
     @pytest.mark.parametrize('tagname', ['tagx11', 'taxg21', 'taxg31'])
     def test_all(self, tagname, token):
@@ -62,11 +64,9 @@ class TestTag:
             res = self.tag.tag_add(tagname, token)
             assert res['errmsg'] == 'created'
             self.tagid = res['tagid']
-        # print(res)
         except Exception as e:
             if '40071' in e.__str__():
                 res = self.tag.tag_list_get(token)
-                # print(res)
                 for i in res['taglist']:
                     if tagname in i.values():
                         self.tagid = i['tagid']
@@ -77,7 +77,7 @@ class TestTag:
         # 查询这个tag是否创建成功
         res = self.tag.tag_get(self.tagid, token)
         assert res['errmsg'] == 'ok'
-        assert res['tagname'] is not None
+        assert res['tagname'] == tagname
         #升级这个tag
         res = self.tag.tag_update(self.tagid, tagname + '1', token)
         assert res['errmsg'] == 'updated'
@@ -89,10 +89,4 @@ class TestTag:
         assert res['errmsg'] == 'deleted'
         # 查询tag是否删除成功
         res = self.tag.tag_get(self.tagid, token)
-        # print(res)
-
-    @pytest.mark.parametrize('tagname', ['tagx11', 'taxg21', 'taxg31'])
-    def test_all2(self, tagname, token):
-        # 新增一个tag
-        res = self.tag.tag_add(tagname, token)
-        print(res)
+        assert res['userlist'] == []
