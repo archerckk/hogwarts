20b4b6b308d44795a8dfcc1eb0094ae56ec966e3
Archer <archerckk@163.com>
2021-02-07 00:02:31 +0800

    修改生成判断条件跟修改生成路径

--- mitmproxy_hw/mitm_demo.py
+++ mitmproxy_hw/mitm_demo.py
@@ -39,33 +39,39 @@ class Template:
 def response(flow: http.HTTPFlow):
     method = flow.request.method
     api= flow.request.path.split('?')[0]
+    tmplist=api.split('/')
+    filename=''.join(tmplist)
+    print(filename)
 
 
+    # if api == '/api/payment/info':
+    tmplist = api.split('/')
+    filename = '_'.join(tmplist)
+    print(filename)
 
-    if api == '/api/payment/info':
-        print('api为：',api)
-        print("测试方法打印：", method.__repr__())
-        url = flow.request.pretty_url.split('?')[0]
+    print('api为：',api)
+    print("测试方法打印：", method.__repr__())
+    url = flow.request.pretty_url.split('?')[0]
 
-        data = get_config_data()['pararms_del']
+    data = get_config_data()['pararms_del']
 
-        for i in data:
-            print(i)
+    for i in data:
+        print(i)
 
-        print('=' * 200)
-        print(flow.request.query.fields)
-        print('=' * 200)
-        params = [{k: v} for k, v in flow.request.query.fields for d in data if k not in d and k.isalpha()==True ]
-        # cookies = [{k: v} for k, v in flow.request.cookies.fields]
+    print('=' * 200)
+    print(flow.request.query.fields)
+    print('=' * 200)
+    params = [{k: v} for k, v in flow.request.query.fields for d in data if k not in d and k.isalpha()==True ]
+    # cookies = [{k: v} for k, v in flow.request.cookies.fields]
 
-        print('params实际内容为：',params)
-        data = {
-            "api": api,
-            "method": method.__repr__(),
-            "params": params,
-        }
+    print('params实际内容为：',params)
+    data = {
+        "api": api,
+        "method": method.__repr__(),
+        "params": params,
+    }
 
-        content=Template.render(addon_dir+"/test2.mustache", data)
-        print("测试代码：",content)
-        with open(r'D:\Hogwarts\mitmproxy_hw\result.yml','w')as f:
-            f.write(content)
+    content=Template.render(addon_dir+"/get.mustache", data)
+    print("测试代码：",content)
+    with open(r'D:\Hogwarts\mitmproxy_hw\api_generated\%s_api.yml'%(filename),'w')as f:
+        f.write(content)
